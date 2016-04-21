bc8a83a4762de35c719e8a6f04f8137b18fecf01
leader22 <y.sugiura.0316@gmail.com>
2016-04-21 14:29:46 +0900

    同じ処理をまとめる

--- lib/editor/src/js/component/EditorCanvas.jsx
+++ lib/editor/src/js/component/EditorCanvas.jsx
@@ -11,11 +11,7 @@ class EditorCanvas extends React.Component {
 
 		if ( !e.target.classList.contains( 'EDT-EditorCanvas__image' ) ) { return; }
 
-		let svg = this.refs.svg;
-		let p   = svg.createSVGPoint();
-		p.x = e.nativeEvent.x | 0;
-		p.y = e.nativeEvent.y | 0;
-		let svgCoord = p.matrixTransform( svg.getScreenCTM().inverse() );
+		let svgCoord = this.getCoordByXY( e.nativeEvent );
 
 		store.dispatch( {
 			type: 'HIGHLIGHT_ADD',
