6c0509f2fbc9232cb5b59a4fbe35c77f7535c355
Makoto OOSUGI <oosugi20@gmail.com>
2017-03-07 02:16:25 +0900

    実際に使う時用のgulpfile.jsサンプルと、適当なmdとimg追加

--- sample/gulpfile.js
+++ sample/gulpfile.js
@@ -1,11 +1,16 @@
 const gulp = require('gulp');
 const plugins = require('gulp-load-plugins')();
+const runSequence = require('run-sequence').use(gulp);
 
 const connect = require('connect');
 const serveStatic = require('serve-static');
 const del = require('del');
 
 const uiSpecMd = require('ui-spec-md');
+
+
+const log = plugins.util.log;
+const colors = plugins.util.colors;
 
 
 
@@ -21,6 +26,9 @@ const uiSpecMd = require('ui-spec-md');
  */
 const BUILD_DIR = './build/';
 
+const LISTEN = 3000;
+const LOCALHOST = 'http://localhost:' + LISTEN;
+
 
 
 /**
@@ -28,9 +36,27 @@ const BUILD_DIR = './build/';
  * @requires del
  */
 gulp.task('clean:html', function (callback) {
-  return del([
-    './build/**/*.html'
-  ], callback);
+	return del([
+		'./build/**/*.html'
+	], callback);
+});
+gulp.task('clean:temp', function (callback) {
+	return del([
+		'./build/_editor',
+		'./build/_editor.html',
+		'./build/_menu.html',
+		'./build/_template',
+	], callback);
+});
+gulp.task('clean:capture', (callback) => {
+	return del([
+		'./build/**/*.+(png|jpg|jpeg|gif)'
+	]);
+});
+gulp.task('clean', function (callback) {
+	return del([
+		'./build/**/*'
+	], callback);
 });
 
 
@@ -41,27 +67,30 @@ gulp.task('clean:html', function (callback) {
  * @requires uiSpecMd
  * @requires BUILD_DIR 展開するディレクトリ。
  */
-gulp.task('build', [
-  'capture',
-  'md'
-]);
+gulp.task('build', (callback) => {
+	runSequence('clean', [
+		'capture',
+		'md'
+	], callback);
+});
 
 
 gulp.task('capture', [
+	'clean:capture'
 ], function () {
-  gulp.src('./src/**/*.@(png|jpg)')
-    .pipe(plugins.plumber())
-    .pipe(gulp.dest(BUILD_DIR));
+	gulp.src('./src/**/*.@(png|jpg|jpeg|gif)')
+		.pipe(plugins.plumber())
+		.pipe(gulp.dest(BUILD_DIR));
 });
 
 
 gulp.task('md', [
-  'clean:html'
+	'clean:html'
 ], function () {
-  gulp.src('./src/**/*.md')
-    .pipe(plugins.plumber())
-    .pipe(uiSpecMd({ srcRoot: __dirname + '/src/' }))
-    .pipe(gulp.dest(BUILD_DIR));
+	gulp.src('./src/**/*.md')
+		.pipe(plugins.plumber())
+		.pipe(uiSpecMd({ srcRoot: __dirname + '/src/' }))
+		.pipe(gulp.dest(BUILD_DIR));
 });
 
 
@@ -74,26 +103,78 @@ gulp.task('md', [
  * @requires BUILD_DIR   ルートとするディレクトリ。
  */
 gulp.task('connect', function () {
-  const app = connect();
+	const app = connect();
 
-  app.use(serveStatic(BUILD_DIR, {
-    index: [ 'index.html' ]
-  }));
+	app.use(serveStatic(BUILD_DIR, {
+		index: [ 'index.html' ]
+	}));
 
-  app.listen(3000);
-
-  plugins.util.log(plugins.util.colors.green('Server started: http://localhost:3000'));
+	app.listen(LISTEN);
 });
 
 
 
 /**
  * 画面設計書編集時用ウォッチタスク。
+ *  - watchして変更になったファイルだけ更新
+ * TODO src内の子ディレクトリ名を変更すると止まるので再起動が必要。
  */
-gulp.task('watch', function () {
-  // `./src/*md` を監視して更新する。
-  gulp.watch('src/**/*.md', [ 'md' ]);
-  gulp.watch('src/**/*.@(png|jpg)', [ 'capture' ]);
+gulp.task('watch', () => {
+	//gulp.watch('src/**/*.md', [ 'md' ]);
+	//gulp.watch('src/**/*.+(png|jpg|jpeg|gif)', [ 'capture' ]);
+	// ↑だと移動とかリネームで2回実行されちゃう。
+	// renamedとdeletedが起きてるからと思われる。
+
+	//なので、一旦src配下全部監視して、
+	gulp.watch('src/**', (event) => {
+
+		// 画像なら
+		if ( /\.(png|jpg|jpeg|gif)$/.test(event.path) ) {
+			let filename = event.path.replace(/^.+\/src\/(.+?)\.(png|jpg|jpeg|gif)$/, '$1.$2')
+			let filedir = filename.split('/').slice(0, -1).join('/');
+
+			// deletedなら削除して、それ以外はコピー
+			// ちなみに、ファイルを移動したときは、deletedとaddedじゃなくて、
+			// deletedとrenamedになる。
+			if (/added|changed|renamed/.test(event.type)) {
+
+				console.log(`------------------------------`);
+				gulp.src(event.path)
+					.pipe(plugins.plumber())
+					.pipe(gulp.dest('./build/' + filedir + '/'))
+					.on('end', () => {
+						log(colors.green('Image Updated!'));
+					});
+
+
+			} else if (/deleted/.test(event.type)){
+
+				del([ './build/' + filename ]);
+
+				//log(colors.red(event.type + ': ') + __dirname + /build/ + filename);
+
+			}
+
+		// mdなら
+		} else if ( /\.md$/.test(event.path) ) {
+
+			if ( /added|renamed/.test(event.type) ) {
+				// たまにイベント多すぎるって言われるけどいいんじゃなかろうか
+				// TODO できれば、全部作り直すタスク実行するんじゃなくて、
+				// 画像同様に変更したファイルだけ処理したいが、uiSpecMdの修正が必要。
+				console.log(`------------------------------`);
+				runSequence(
+					'clean:temp',
+					'md',
+					() => {
+						log(colors.green('HTML Updated!'));
+					}
+				);
+			}
+
+		}
+
+	});
 });
 
 
@@ -103,11 +184,22 @@ gulp.task('watch', function () {
  *
  * @requires gulp md
  * @requires gulp connect
- *
- * @todo ./src/*.mdの更新を監視する
  */
 gulp.task( 'start', [
-  'build',
-  'connect',
-  'watch'
-] );
+], () => {
+
+	runSequence(
+		'build',
+		'connect',
+		'watch',
+		() => {
+			console.log(`
+------------------------------
+Server started: ` + colors.green(LOCALHOST) + `
+------------------------------
+Watching files...
+`);
+		}
+	);
+
+} );
