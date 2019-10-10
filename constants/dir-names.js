8073b67e849ba64302231ff8e6c74dd7c649cee6
tomof <fffjt007@gmail.com>
2019-10-10 16:43:20 +0900

    modify .gitignore built-in-template to default-theme

--- constants/dir-names.js
+++ constants/dir-names.js
@@ -1,4 +1,4 @@
 module.exports = Object.freeze({
-  DEFAULT_THEME: 'default-theme',
+  DEFAULT_THEME: 'default-theme', // It also specified in .gitignore. If change this, please check .gitignore.
   THEME_ASSETS: 'assets',
 })
