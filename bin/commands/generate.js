6c4147d834d3b35d97cd2e57d0abb989409a5ba8
tomof <fffjt007@gmail.com>
2020-01-17 09:29:41 +0900

    implemented converting process absolute paths to relative to see on browser local

--- bin/commands/generate.js
+++ bin/commands/generate.js
@@ -12,12 +12,20 @@ exports.builder = yargs => {
     ...mdDir,
     ...destDir,
     ...themeDir,
+    l: {
+      alias: 'local',
+      demandOption: false,
+      requiresArg: false,
+      describe:
+        'Convert all absolute paths to relative paths so that the generated file can be viewed locally on a browser with "file: //".',
+    },
   })
 }
 
 exports.handler = async argv => {
   await generateSpecAndTree(argv.mdDir, argv.destDir, {
     isEditable: false,
+    canSeeLocal: argv.local,
     themeDir: argv.themeDir,
   })
 }
